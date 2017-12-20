a83e3aeaafdc499a60d384dcf2eb714e46a0e7bb
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-12-19 18:10:00 -0800

    Add ReverseString

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What string would you like to be reversed?");
+            string input = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(StReverse(input));
+            Console.Read();
+            
+        }
+
+        static string StReverse(string input)
+        {
+            string reversed = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversed = reversed.Insert(0, input[i].ToString());
+            }
+            return reversed;
+        }
+    }
+}
